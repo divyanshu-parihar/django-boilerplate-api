9067c61f7f53416c094a216063ca572a6da30f58
divyanshu1_parihar <divyanshu1447@gmail.com>
2020-12-11 18:30:25 +0530

    get_post_ADDED

--- apiV2/views.py
+++ apiV2/views.py
@@ -1,14 +1,42 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .models import students
+from .serializers import studentSerializers
+
+#<------------------ function based view----------------->
 
 
 
-# function based view
+
 # which is handled by imported in rest_framework Response
-@api_view(['GET','POST']) # note --> by default GET is passed as list .
-#api_view imported to see everything in the browser!
-def homeApi(request): #normal view 
+# @api_view(['GET','POST']) # note --> by default GET is passed as list .
+# #api_view imported to see everything in the browser!
+# def homeApi(request): #normal view 
+# 	if request.method == 'POST':
+# 		return Response({'msg':'THIS IS A POST REQUEST','data':request.data})
+	
+# 	return Response({'msg':'THIS IS A GET REQUEST','data':request.data})
+#<------------------ function based view ENDING----------------->
+
+#<------------------ CRUD FUNCTIONALITY----------------->
+@api_view(['GET','POST','PUT','DELETE'])
+def homeApi(request):
+	if request.method == 'GET':
+		id = request.data.get('id')
+		if id is not None:
+			student = students.objects.all(id= id)
+			serialzer = studentSerializers(student)
+			return Response(serialzer.data)
+		else:
+			student = students.objects.all()
+			serializer = studentSerializers(student ,many = True)
+			return Response(serializer.data)
+	
 	if request.method == 'POST':
-		return Response({'msg':'THIS IS A POST REQUEST'})
-	return Response({'msg':'THIS IS A GET REQUEST'})
+		data = request.data
+		serializer = studentSerializers(data=data)
+		if serializer.is_valid():
+			serializer.save()
+			return Response({'msg':serializer.data})
+		return Response({'msg':serializer.errors})
--- myapp.py
+++ myapp.py
@@ -3,14 +3,16 @@ import json
 url = 'http://127.0.0.1:8000/api/'
 
 data = {
-'id':'5',
-'name':'happy singh',
-'age':13,
-'phone_number':888888888888,
+'id':6,
+'name':'aditya singh',
+'age':14,
+'phone_number':788888888888,
 'father_name':'xyz'
 }
 # we have to made a header to specify the content-type
-header = {'contenc-Type':"application/json"}
+header = {
+    'content-Type':"application/json"
+}
 
 json_data=json.dumps(data)
 # r= requests.put(url= url, data= json_data)
@@ -18,4 +20,5 @@ json_data=json.dumps(data)
 
 # r= requests.delete(url= url, data= json_data)
 r= requests.post(headers= header,url= url,data = data)
-print(r.text) 
+data = r.json()
+print(data) 
