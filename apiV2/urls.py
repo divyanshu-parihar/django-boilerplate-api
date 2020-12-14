a63ff0dda3ceeafcabeb2b205b1e5fe344908123
divyanshu1_parihar <divyanshu1447@gmail.com>
2020-12-14 12:16:38 +0530

    'added_read_only_ModelViewSet'

--- apiV2/urls.py
+++ apiV2/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path,include
-from .views import studentModelViewSet
+from .views import studentReadOnlyModelViewSet
 from rest_framework.routers import DefaultRouter
 
 
@@ -7,7 +7,7 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 
 
-router.register('student',studentModelViewSet,basename='student')
+router.register('student',studentReadOnlyModelViewSet,basename='student')
 urlpatterns = [
     path('',include(router.urls)),
 ]
--- apiV2/views.py
+++ apiV2/views.py
@@ -4,6 +4,10 @@ from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.response import Response
 
-class studentModelViewSet(viewsets.ModelViewSet):#only change the inherited class it will haange everyting by itserlf.
-	queryset = students.objects.all()
-	serializer_class = studentSerializers
+# class studentModelViewSet(viewsets.ModelViewSet):#only change the inherited class it will haange everyting by itserlf.
+# 	queryset = students.objects.all()
+# 	serializer_class = studentSerializers
+# read only model viewset
+class studentReadOnlyModelViewSet(viewsets.ReadOnlyModelViewSet):
+ 	queryset = students.objects.all()
+ 	serializer_class = studentSerializers
