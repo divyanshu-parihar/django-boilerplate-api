f199dd94088a385207147f5f3a84c00d61f7ae0b
divyanshu1_parihar <divyanshu1447@gmail.com>
2020-12-14 14:57:25 +0530

    'add_comment_function_based_view'

--- apiV2/views.py
+++ apiV2/views.py
@@ -4,6 +4,16 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import IsAuthenticated,IsAdminUser,IsAuthenticatedOrReadOnly,DjangoModelPermissions,DjangoModelPermissionsOrAnonReadOnly
 from .custompermission import MyPermission
+# for function based views
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import authentication_classes,permission_classes
+
+# add @authentication_classes(Authentication)
+# @permission_classes(IsAuthenticated)
+
+
+
 class studentModelViewSet(ModelViewSet):
 	queryset = students.objects.all()
 	serializer_class = studentSerializers
