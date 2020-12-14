cf6b9d347acfce7a50cf6ca08181fc035bd07fb1
divyanshu1_parihar <divyanshu1447@gmail.com>
2020-12-14 12:08:11 +0530

    'ModelViewSet_implemented'

--- apiV2/urls.py
+++ apiV2/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path,include
-from .views import studentList
+from .views import studentModelViewSet
 from rest_framework.routers import DefaultRouter
 
 
@@ -7,7 +7,7 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 
 
-router.register('',studentList,basename='student')
+router.register('student',studentModelViewSet,basename='student')
 urlpatterns = [
     path('',include(router.urls)),
 ]
--- apiV2/views.py
+++ apiV2/views.py
@@ -4,22 +4,6 @@ from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.response import Response
 
-class studentList(viewsets.ViewSet):#only change the inherited class it will haange everyting by itserlf.
-	def get(self,request):
-		student = students.objects.all()
-		serializer= studentSerializers(student,many= True)
-		return Response(serializer.data)
-
-
-	def retrieve(self,request,pk):
-		id= pk
-		if id is not None:
-			stu = students.objects.get(id= id)
-			serializer = studentSerializers(stu)
-			return Response(serializer.data)
-	
-	def create(self,request):
-		serializer = studentSerializers(data= request.data)
-		if serializer.is_valid():
-			return Response({'msg' : 'Data created'},status= status.HTTP_201_CREATED)
-		return Response(serializer.errors,status = status.HTTP_400_BAD_REQUEST)
+class studentModelViewSet(viewsets.ModelViewSet):#only change the inherited class it will haange everyting by itserlf.
+	queryset = students.objects.all()
+	serializer_class = studentSerializers
